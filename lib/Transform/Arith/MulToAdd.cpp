4edfd76d14b3d5b71fcef0847e86e15d88f956c3
Marton Moro <moro.marton@gmail.com>
2026-03-16 14:47:04 +0100

    writing our first pass

---
+++ lib/Transform/Arith/MulToAdd.cpp
@@ -0,0 +1,116 @@
+#include "lib/Transform/Arith/MulToAdd.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/include/mlir/Pass/Pass.h"
+
+namespace mlir
+{
+    namespace tutorial
+    {
+
+        using arith::AddIOp;
+        using arith::ConstantOp;
+        using arith::MulIOp;
+
+        // Replace y = C*x with y = C/2*x + C/2*x, when C is a power of 2, otherwise do
+        // nothing.
+        struct PowerOfTwoExpand : public OpRewritePattern<MulIOp>
+        {
+            PowerOfTwoExpand(mlir::MLIRContext *context)
+                : OpRewritePattern<MulIOp>(context, /*benefit=*/2) {}
+
+            LogicalResult matchAndRewrite(MulIOp op,
+                                          PatternRewriter &rewriter) const override
+            {
+                Value lhs = op.getOperand(0);
+
+                // canonicalization patterns ensure the constant is on the right, if there is a constant
+                // See https://mlir.llvm.org/docs/Canonicalization/#globally-applied-rules
+                Value rhs = op.getOperand(1);
+                auto rhsDefiningOp = rhs.getDefiningOp<arith::ConstantIntOp>();
+                if (!rhsDefiningOp)
+                {
+                    return failure();
+                }
+
+                int64_t value = rhsDefiningOp.value();
+                bool is_power_of_two = (value & (value - 1)) == 0;
+
+                if (!is_power_of_two)
+                {
+                    return failure();
+                }
+
+                ConstantOp newConstant = rewriter.create<ConstantOp>(
+                    rhsDefiningOp.getLoc(), rewriter.getIntegerAttr(rhs.getType(), value / 2));
+                MulIOp newMul = rewriter.create<MulIOp>(op.getLoc(), lhs, newConstant);
+                AddIOp newAdd = rewriter.create<AddIOp>(op.getLoc(), newMul, newMul);
+
+                rewriter.replaceOp(op, newAdd);
+                rewriter.eraseOp(rhsDefiningOp);
+
+                return success();
+            }
+        };
+
+        // Replace y = 9*x with y = 8*x + x
+        struct PeelFromMul : public OpRewritePattern<MulIOp>
+        {
+            PeelFromMul(mlir::MLIRContext *context)
+                : OpRewritePattern<MulIOp>(context, /*benefit=*/1) {}
+
+            LogicalResult matchAndRewrite(MulIOp op,
+                                          PatternRewriter &rewriter) const override
+            {
+                Value lhs = op.getOperand(0);
+                Value rhs = op.getOperand(1);
+                auto rhsDefiningOp = rhs.getDefiningOp<arith::ConstantIntOp>();
+                if (!rhsDefiningOp)
+                {
+                    return failure();
+                }
+
+                int64_t value = rhsDefiningOp.value();
+
+                // We are guaranteed `value` is not a power of two, because the greedy
+                // rewrite engine ensures the PowerOfTwoExpand pattern is run first, since
+                // it has higher benefit.
+
+                ConstantOp newConstant = rewriter.create<ConstantOp>(
+                    rhsDefiningOp.getLoc(), rewriter.getIntegerAttr(rhs.getType(), value - 1));
+                MulIOp newMul = rewriter.create<MulIOp>(op.getLoc(), lhs, newConstant);
+                AddIOp newAdd = rewriter.create<AddIOp>(op.getLoc(), newMul, lhs);
+
+                rewriter.replaceOp(op, newAdd);
+                rewriter.eraseOp(rhsDefiningOp);
+
+                return success();
+            }
+        };
+
+        void MulToAddPass::runOnOperation()
+        {
+            mlir::RewritePatternSet patterns(&getContext());
+            patterns.add<PowerOfTwoExpand>(&getContext());
+            patterns.add<PeelFromMul>(&getContext());
+            (void)applyPatternsGreedily(getOperation(), std::move(patterns));
+        }
+
+    } // namespace tutorial
+} // namespace mlir
+
+// NOTE(mmoro): swapping the benefits resulted in only peeling
+/*
+func.func @power_of_two_plus_one(%arg0: i32) -> i32 {
+    %0 = arith.addi %arg0, %arg0 : i32
+    %1 = arith.addi %0, %arg0 : i32
+    %2 = arith.addi %1, %arg0 : i32
+    %3 = arith.addi %2, %arg0 : i32
+    %4 = arith.addi %3, %arg0 : i32
+    %5 = arith.addi %4, %arg0 : i32
+    %6 = arith.addi %5, %arg0 : i32
+    %7 = arith.addi %6, %arg0 : i32
+    return %7 : i32
+  }
+*/
